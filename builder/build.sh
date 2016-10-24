e2ce3fee0497edb78d05b987d154688e28f04a84
Sumi Minano <sumi.minano@gmail.com>
2016-10-24 01:52:52 +0000

    riot

--- builder/build.sh
+++ builder/build.sh
@@ -3,18 +3,23 @@
 # dir vars
 builderdir=$(cd $(dirname ${0}); pwd)
 appdir=$(dirname ${builderdir})
+releasedir=${appdir}/releases
 
-${builderdir}/setup.sh
+source ${builderdir}/setup.sh
 
 # settings
 electron_version=$(electron --version | sed -e 's/^v//')
 package_path=${appdir}/package.json
 appname=$( (grep productName ${package_path} || grep name ${package_path}) | sed -e 's/[^:]\+: *"\([^"]\+\)".*/\1/' )
 
-# build
-#electron-packager ${appdir} --platform=all --arch=all --version=${electron_version}
-#electron-packager ${appdir} --platform=win32 --arch=x64 --version=${electron_version}
-electron-packager ${appdir} --platform=linux --arch=x64 --version=${electron_version}
+mkdir -p ${releasedir}
 
 # clean
-#rm -rf node_modules ${appname}-*
+rm -rf ${releasedir}/${appname}-*
+
+# build
+cd ${releasedir}
+#electron-packager ${appdir} --platform=all --arch=all --version=${electron_version}
+#electron-packager ${appdir} --platform=win32 --arch=x64 --version=${electron_version}
+electron-packager ${appdir} --platform=linux --arch=x64 --version=${electron_version} \
+    --ignore '(node_modules|releases|builder|README.md|bower.json)'
--- builder/setup.sh
+++ builder/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# usage
+#   source setup.sh
+
 # dir vars
 builderdir=$(cd $(dirname ${0}); pwd)
 appdir=$(dirname ${builderdir})
 
 # setup build environment
-cd ${builderdir}
+cd ${appdir}
+npm install builder
+PATH+=:${appdir}/node_modules/.bin
+
 npm install
-PATH+=:./node_modules/.bin
+
+typeset -U path > /dev/null 2>&1 || true
